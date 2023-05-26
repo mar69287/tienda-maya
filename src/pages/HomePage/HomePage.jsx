cc7f09d7b9f9fad9e9e853103c825c417a80c986
mar69287 <marcoa.ruiz97@gmail.com>
2023-05-26 14:29:39 -0700

    create navbar skeleton and logo

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import logo from '../../assets/tienda-image.png'
+import { Image } from '@chakra-ui/react'
+
+
+const HomePage = () => {
+  return (
+    <div>Hi</div>
+  )
+}
+
+export default HomePage

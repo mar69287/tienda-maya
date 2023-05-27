17e585f1dd955a32c2362e8425595b622ea489a6
mar69287 <marcoa.ruiz97@gmail.com>
2023-05-27 10:12:57 -0700

    responsive about card

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -6,15 +6,15 @@ const AboutCard = () => {
   return (
     <Stack direction={['column', 'column', 'row']} w='100%' p={['3rem 1.4rem', '4rem 3.5rem']} bg="rgb(233, 227, 224)" spacing={{ base: 0, md: 3, lg: 10 }}>
       <VStack justifyContent='center' alignItems='center' mb={{ base: '2rem', md: '0' }} w={['100%', '100%', '50%']}>
-        <Box w={['90%', '80%']} mb={'2.5rem'}>
+        <Box w={['90%', '80%']}>
           <Heading marginBottom='1.5rem' textAlign='center'>Journey to the Heart of Mayan Artistry</Heading>
           <Text textAlign='center'>With deep roots in tradition, every piece we offer is meticulously crafted using centuries-old techniques passed down from mothers to daughters for generations. By celebrating their remarkable craftsmanship, Tienda Maya becomes a bridge between cultures, honoring Guatemala's vibrant heritage and the enduring spirit of the Mayan people.</Text>
         </Box>
-          <Button colorScheme='green' variant='outline'>Shop Now</Button>
+          {/* <Button colorScheme='green' variant='outline'>Shop Now</Button> */}
       </VStack>
       <HStack w={['100%', '100%', '50%']} spacing={5} justifyContent={'center'}>
-        <Img src={about1} w={{base: '45%', md: '45%', lg: '50%'}} h={['20rem', '20rem', '25rem', '35rem']} />
-        <Img src={about2} w={{base: '45%', md: '45%', lg: '50%'}} h={['20rem', '20rem', '25rem', '35rem']} />
+        <Img src={about1} w={['45%', '45%', '42%', '38%']} h={['20rem', '20rem', '22rem', '30rem']} />
+        <Img src={about2} w={['45%', '45%', '42%', '38%']} h={['20rem', '20rem', '22rem', '30rem']} />
       </HStack>
     </Stack>
   );

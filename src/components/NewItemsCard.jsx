4f598d6d761fb4cbec7a554990c5123ec6cb85fb
mar69287 <marcoa.ruiz97@gmail.com>
2023-06-27 13:35:52 -0700

    Create link to products page

--- src/components/NewItemsCard.jsx
+++ src/components/NewItemsCard.jsx
@@ -1,34 +1,46 @@
-import { Box, Heading, Image, SimpleGrid, Text, VStack } from '@chakra-ui/react';
+import { Box, Heading, Image, SimpleGrid, Text, VStack, Stack } from '@chakra-ui/react';
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const NewItemsCard = ({ products }) => {
   const newProducts = products.slice(-4);
 
   return (
-    <VStack width={{ xl: '100%', '2xl': '1400px' }} m='0px auto 3rem auto'>
-        <Heading as="h2" size="lg" mb={4} textAlign={'center'}>
-            New Items
-        </Heading>
-        <SimpleGrid columns={4} spacing={6} w={'100%'}>
-            {newProducts.map(product => 
-                <Box key={product.id} width='100%'>
-                    <Image
-                        w={'100%'} 
-                        h={'20rem'}
-                        objectFit={'cover'}
-                        src={product.image}
-                        mb={'1rem'}
-                    />
-                    <Heading as="h3" size="sm" textAlign={'center'}>
-                        {product.title}
-                    </Heading>
-                    <Text textAlign={'center'}>
-                        ${product.price}
+    <>
+        <Stack width={{ xl: '100%', '2xl': '1400px' }} m='0px auto 0px auto'>
+            <Box _hover={{ cursor: 'pointer', color: 'rgb(255, 160, 76)', transition: 'color 0.2s ease-in-out' }}>
+                <Link to='/products'>
+                    <Text fontSize={'1.2rem'} fontWeight={'bold'}>
+                        Shop All Items
                     </Text>
-                </Box>
-            )}
-        </SimpleGrid >
-    </VStack>
+                </Link>
+            </Box>
+        </Stack>
+        <VStack width={{ xl: '100%', '2xl': '1400px' }} m='0px auto 3rem auto'>
+            <Heading as="h2" size="lg" mb={4} textAlign={'center'} mt={'0rem'}>
+                New Items
+            </Heading>
+            <SimpleGrid columns={4} spacing={6} w={'100%'}>
+                {newProducts.map(product => 
+                    <Box key={product.id} width='100%'>
+                        <Image
+                            w={'100%'} 
+                            h={'20rem'}
+                            objectFit={'cover'}
+                            src={product.image}
+                            mb={'1rem'}
+                        />
+                        <Heading as="h3" size="sm" textAlign={'center'}>
+                            {product.title}
+                        </Heading>
+                        <Text textAlign={'center'}>
+                            ${product.price}
+                        </Text>
+                    </Box>
+                )}
+            </SimpleGrid >
+        </VStack>
+    </>
   )
 }
 

cac22ea42da54a1ce88d003fa1c4d302cc08abf2
mar69287 <marcoa.ruiz97@gmail.com>
2023-06-27 14:09:09 -0700

    Adjust responsiveness of Benefits Grid

--- src/components/BenefitsGrid.jsx
+++ src/components/BenefitsGrid.jsx
@@ -6,8 +6,8 @@ import { HiHome } from 'react-icons/hi';
 
 const BenefitsGrid = () => {
   return (
-    <Grid as='section' templateColumns={{ xl: 'repeat(3, 1fr)', '2xl': 'repeat(6, 1fr)', }} m='0px auto 3rem auto' width={{ xl: '100%', '2xl': '1400px' }}>
-      <GridItem colSpan={{ xl: 2, '2xl': 2 }}>
+    <Grid as='section' templateColumns={{ md: 'repeat(3, 1fr)', 'md': 'repeat(6, 1fr)', }} m='0px auto 3rem auto' width={{ xl: '100%', '2xl': '1400px' }}>
+      <GridItem colSpan={{ md: 2, '2xl': 2 }}>
         <Box p={6} border="1px solid" borderColor="black">
           <VStack>
             <TbHeartHandshake size={40} color="black" />
@@ -20,7 +20,7 @@ const BenefitsGrid = () => {
           </VStack>
         </Box>
       </GridItem>
-      <GridItem colSpan={{ xl: 2, '2xl': 2 }}>
+      <GridItem colSpan={{ md: 2, '2xl': 2 }}>
         <Box p={6} border="1px solid" borderColor="black">
           <VStack>
             <BiRecycle size={40} color="black" />
@@ -33,7 +33,7 @@ const BenefitsGrid = () => {
           </VStack>
         </Box>
       </GridItem>
-      <GridItem colSpan={{ xl: 2, '2xl': 2 }}>
+      <GridItem colSpan={{ md: 2, '2xl': 2 }}>
         <Box p={6} border="1px solid" borderColor="black">
           <VStack>
               <GiHandBandage size={40} color="black" />
@@ -46,7 +46,7 @@ const BenefitsGrid = () => {
           </VStack>
         </Box>
       </GridItem>
-      <GridItem colSpan={{ xl: 3, '2xl': 3 }}>
+      <GridItem colSpan={{ sm: 1, 'md': 3 }}>
         <Box p={6} border="1px solid" borderColor="black">
           <VStack>
               <GiFeather size={40} color="black" />
@@ -59,7 +59,7 @@ const BenefitsGrid = () => {
           </VStack>
         </Box>
       </GridItem>
-      <GridItem colSpan={{ xl: 3, '2xl': 3 }}>
+      <GridItem colSpan={{ sm: 1, 'md': 3 }}>
         <Box p={6} border="1px solid" borderColor="black">
           <VStack>
               <HiHome size={40} color="black" />

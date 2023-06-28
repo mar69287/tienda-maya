adf35d4e654db8b4bd66bfe029b20412f4d57285
mar69287 <marcoa.ruiz97@gmail.com>
2023-06-28 16:41:06 -0700

    Add hover animation to product cards

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,26 +1,30 @@
 import { Box, Heading, Image, Text } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
-const ProductCard = ({product}) => {
+const ProductCard = ({ product }) => {
   return (
     <Link to={`/products/${product.title}`}>
-      <Box width='100%'>
-          <Image
-              w={'100%'}
-              h={'20rem'}
-              objectFit={'cover'}
-              src={product.image}
-              mb={'1rem'}
-          />
-          <Heading as="h3" size="sm" textAlign={'center'}>
-              {product.title}
-          </Heading>
-          <Text textAlign={'center'}>
-              ${product.price}
-          </Text>
+      <Box
+        width="100%"
+        transition="border-color 0.3s ease"
+        _hover={{ borderColor: 'rgb(255, 160, 76)' }}
+        border="1px solid transparent"
+        overflow="hidden"
+      >
+        <Image
+          w="100%"
+          h="20rem"
+          objectFit="cover"
+          src={product.image}
+          mb="1rem"
+        />
+        <Heading as="h3" size="sm" textAlign="center">
+          {product.title}
+        </Heading>
+        <Text textAlign="center">${product.price}</Text>
       </Box>
     </Link>
-  )
+  );
 }
 
-export default ProductCard
+export default ProductCard;

8ca8ddbe4c72c593dd8144406a2ae8c986b3a44e
mar69287 <marcoa.ruiz97@gmail.com>
2023-06-28 16:30:05 -0700

    Add quantity to detail page

--- src/pages/ProductDetailPage/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage/ProductDetailPage.jsx
@@ -1,19 +1,35 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from 'react'
 import { getProduct } from "../../utilities/products-api";
-import { Box, SimpleGrid, Image, Heading, Text, VStack, Button} from "@chakra-ui/react";
+import { Box, SimpleGrid, Image, Heading, Text, VStack, Button, HStack, Spinner } from "@chakra-ui/react";
 
 const ProductDetailPage = () => {
     const { productId } = useParams();
     const [product, setProduct] = useState(null);
+    const [quantity, setQuantity] = useState(1);
 
     useEffect(() => {
         async function getProductById() {
             const product = await getProduct(productId);
             setProduct(product);
+            
         }
         getProductById();
     }, []);
+
+    if (!product) {
+        return <Spinner />; 
+    }
+
+    const handleDecreaseQuantity = () => {
+        if (quantity > 1) {
+          setQuantity(quantity - 1);
+        }
+      };
+    
+      const handleIncreaseQuantity = () => {
+        setQuantity(quantity + 1);
+      };
 
 
   return (
@@ -47,7 +63,11 @@ const ProductDetailPage = () => {
                 </Text>
                 ))}
             </VStack>
-           
+            <HStack mb={6} border={'1px solid black'} display="inline-flex">
+                <Button onClick={handleDecreaseQuantity} variant='ghost' borderRadius={0}  _hover={{ bg: 'RGBA(0, 0, 0, 0.24)' }} transition='background-color 0.3s ease'>-</Button>
+                <Text>{quantity}</Text>
+                <Button onClick={handleIncreaseQuantity} variant='ghost' borderRadius={0}  _hover={{ bg: 'RGBA(0, 0, 0, 0.24)' }} transition='background-color 0.3s ease'>+</Button>
+            </HStack>
             <Button w={'100%'} bg='rgb(255, 160, 76)' color='white' variant='outline' _hover={{ bg: 'rgb(230, 137, 50)' }} transition='background-color 0.3s ease' >
                 ADD TO CART
             </Button>

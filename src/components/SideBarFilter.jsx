c29f28c7babb2ba767e0198d22db7f843028e097
mar69287 <marcoa.ruiz97@gmail.com>
2023-06-27 18:59:12 -0700

    Create filtering products by price

--- src/components/SideBarFilter.jsx
+++ src/components/SideBarFilter.jsx
@@ -1,20 +1,37 @@
-import { VStack, Heading, Text } from "@chakra-ui/react";
+import { VStack, Heading, Text, Input, Button, Flex } from "@chakra-ui/react";
 import { Link } from 'react-router-dom';
 
-const SideBarFilter = () => {
-    const categories = ["Kitchen", "Clothing"];
+const SideBarFilter = ({ onPriceRangeChange }) => {
+  const categories = ["Kitchen", "Clothing"];
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const min = parseInt(event.target.elements.min.value);
+    const max = parseInt(event.target.elements.max.value);
+    // console.log(min,max)
+    onPriceRangeChange(min, max);
+  };
 
   return (
-        <VStack align='start' spacing={4} py={6} >
-          <Heading as="h3" size="md">Categories</Heading>
-          {categories.map(category => (
-                <Link key={category} to={`/products/${category}`} >
-                    <Text _hover={{ cursor: 'pointer', color: 'rgb(255, 160, 76)', transition: 'color 0.2s ease-in-out' }} paddingLeft={'.4rem'}>{category}</Text>
-                </Link>
-            ))}
-          <Heading as="h3" size="md">Price Range</Heading>
-        </VStack>
+    <VStack align='start' spacing={4} py={6}>
+      <Heading as="h3" size="md">Categories</Heading>
+      {categories.map(category => (
+        <Link key={category} to={`/products/${category}`} >
+          <Text _hover={{ cursor: 'pointer', color: 'rgb(255, 160, 76)', transition: 'color 0.2s ease-in-out' }} paddingLeft={'.4rem'}>{category}</Text>
+        </Link>
+      ))}
+      <Heading as="h3" size="md">Price Range</Heading>
+      <form onSubmit={handleFormSubmit}>
+        <Flex>
+          <Input type="number" name="min" placeholder="$ Min" marginRight="1rem" />
+          <Input type="number" name="max" placeholder="$ Max" marginRight="1rem" />
+          <Button paddingX={'2rem'} type="submit" colorScheme="teal">Apply</Button>
+        </Flex>
+      </form>
+    </VStack>
   )
 }
 
-export default SideBarFilter
+export default SideBarFilter;
+
+
--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -7,30 +7,51 @@ import SideBarFilter from "../../components/SideBarFilter";
 
 const CategoryPage = () => {
   const { category } = useParams();
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [priceRange, setPriceRange] = useState({ min: '', max: '' });
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
     async function getProductsByCategory() {
-            const products = await getCategory(category);
-            setProducts(products);
-        }
-        getProductsByCategory();
-    }, [products]);
+      const categoryProducts = await getCategory(category);
+      setProducts(categoryProducts);
+    }
+    getProductsByCategory();
+  }, [category]);
+
+  const handlePriceRangeChange = (min, max) => {
+    const filtered = products.filter(product => (
+      product.price >= min && product.price <= max
+    ));
+    console.log(filtered)
+    setFilteredProducts(filtered);
+  };
+
+  useEffect(() => {
+    if (priceRange.min === '' && priceRange.max === '') {
+      setFilteredProducts(products); // Reset filteredProducts to all products
+    } else {
+      const filtered = products.filter(product => (
+        product.price >= priceRange.min && product.price <= priceRange.max
+      ));
+      setFilteredProducts(filtered);
+    }
+  }, [products, priceRange]);
 
   return (
-    <Grid as='section' templateColumns={{ md: 'repeat(6, 1fr)', }} m='1rem auto 3rem auto' width={{ xl: '100%', '2xl': '1400px' }}>
-        <GridItem colSpan={{ md: 2}} h={'100%'} borderRight={'1px black solid'} paddingRight={'3rem'}>
-            <SideBarFilter />
-        </GridItem>
-        <GridItem colSpan={{ md: 4}} paddingLeft={'3rem'}>
-            <SimpleGrid  columns={[1, 2, 3]} spacing={6} w={'100%'}>
-                {products.map(product => 
-                    <ProductCard key={product.id} product={product} />
-                )}
-            </SimpleGrid >
-        </GridItem>
+    <Grid as='section' templateColumns={{ md: 'repeat(6, 1fr)' }} m='1rem auto 3rem auto' width={{ xl: '100%', '2xl': '1400px' }}>
+      <GridItem colSpan={{ md: 2 }} h='100%' borderRight='1px black solid' paddingRight='3rem'>
+        <SideBarFilter onPriceRangeChange={handlePriceRangeChange} />
+      </GridItem>
+      <GridItem colSpan={{ md: 4 }} paddingLeft='3rem'>
+        <SimpleGrid columns={[1, 2, 3]} spacing={6} w='100%'>
+          {filteredProducts.map(product =>
+            <ProductCard key={product.id} product={product} />
+          )}
+        </SimpleGrid>
+      </GridItem>
     </Grid>
   )
 }
 
-export default CategoryPage
+export default CategoryPage;

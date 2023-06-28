c6976fd56649a9e1854a04d269d7a2f1b4d04c4e
mar69287 <marcoa.ruiz97@gmail.com>
2023-06-27 17:25:20 -0700

    Display items in category page

--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -1,7 +1,9 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from 'react'
 import { getCategory } from "../../utilities/products-api";
-
+import { Grid, GridItem, SimpleGrid } from "@chakra-ui/react";
+import ProductCard from "../../components/ProductCard";
+import SideBarFilter from "../../components/SideBarFilter";
 
 const CategoryPage = () => {
   const { category } = useParams();
@@ -13,10 +15,21 @@ const CategoryPage = () => {
             setProducts(products);
         }
         getProductsByCategory();
-    }, []);
+    }, [products]);
 
   return (
-    <div>CategoryPage</div>
+    <Grid as='section' templateColumns={{ md: 'repeat(6, 1fr)', }} m='1rem auto 3rem auto' width={{ xl: '100%', '2xl': '1400px' }}>
+        <GridItem colSpan={{ md: 2}} h={'100%'} borderRight={'1px black solid'} paddingRight={'3rem'}>
+            <SideBarFilter />
+        </GridItem>
+        <GridItem colSpan={{ md: 4}} paddingLeft={'3rem'}>
+            <SimpleGrid  columns={[1, 2, 3]} spacing={6} w={'100%'}>
+                {products.map(product => 
+                    <ProductCard key={product.id} product={product} />
+                )}
+            </SimpleGrid >
+        </GridItem>
+    </Grid>
   )
 }
 

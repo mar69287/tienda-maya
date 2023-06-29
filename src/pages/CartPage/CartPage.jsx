79f5254d317ebfcb7330f3ffaaf41f8caccd7dcb
mar69287 <marcoa.ruiz97@gmail.com>
2023-06-28 20:23:11 -0700

    Install stripe checkout

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,9 +1,11 @@
-import { Box, VStack, Text, Button, Grid, GridItem } from '@chakra-ui/react'
+import { Box, VStack, Text, Button, HStack, Spacer } from '@chakra-ui/react'
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import CartItemCard from '../../components/CartItemCard';
+import StripeCheckout from "react-stripe-checkout";
 
 const CartPage = ({ user, cart, setCart, setCountCart }) => {
+    const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
     useEffect(() => {
         let totalQuantity = 0;
@@ -22,6 +24,35 @@ const CartPage = ({ user, cart, setCart, setCountCart }) => {
                 {cart.map((item) => {
                     return <CartItemCard key={item.id} item={item} cart={cart} setCart={setCart}/>
                 })}
+                <VStack marginY={10} mx={4}>
+                <HStack w="100%">
+                  <Spacer />
+                  <Text fontSize="lg" fontWeight="bold" mb={2}>
+                    Total: ${total.toFixed(2)}
+                  </Text>
+                </HStack>
+                <HStack w="100%" paddingBottom={"4"}>
+                  <Spacer />
+                  <Link to="/products">
+                    <Button colorScheme="gray" size="md">
+                      Continue Shopping
+                    </Button>
+                  </Link>
+                  {/* <StripeCheckout
+                    stripeKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY || ""}
+                    token={handleToken}
+                    currency="USD"
+                    name="Fresh Finds"
+                  >
+                    <Button  colorScheme="blue" size="md" >
+                      Purchase
+                    </Button>
+                  </StripeCheckout> */}
+                </HStack>
+                <Text>
+                  (Demo Card Number: 4242 4242 4242 4242 exp: 12/34 CVC: 123)
+                </Text>
+              </VStack>
             </>
         ) : ( 
             <VStack marginY={10} >

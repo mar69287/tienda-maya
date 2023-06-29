b0091c1c6f7f4dab1ed63a78aeadc66035f02a83
mar69287 <marcoa.ruiz97@gmail.com>
2023-06-29 15:47:01 -0700

    Responsive product detail page

--- src/pages/ProductDetailPage/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage/ProductDetailPage.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from 'react'
 import { getProduct } from "../../utilities/products-api";
-import { Box, SimpleGrid, Image, Heading, Text, VStack, Button, HStack, Spinner, useToast } from "@chakra-ui/react";
+import { Box, SimpleGrid, Image, Heading, Text, VStack, Button, HStack, Spinner, useToast, Show } from "@chakra-ui/react";
 import { Link } from 'react-router-dom';
 
 const ProductDetailPage = ({ cart, setCart, setCountCart }) => {
@@ -72,51 +72,55 @@ const ProductDetailPage = ({ cart, setCart, setCountCart }) => {
     };
 
   return (
-    <SimpleGrid columns={2} spacing={10} width={{ xl: '100%', '2xl': '1400px' }} m='0px auto 0px auto' paddingTop={'3rem'}>
-        <Box width='100%' paddingTop={'2rem'}>
-          <Image
-              w={'100%'}
-              h={'38rem'}
-              objectFit={'center'}
-              src={product.image}
-              mb={'1rem'}
-          />
-        </Box>
-        <Box >
-            <Heading as="h2" size="xl" mb={6} textTransform="uppercase" fontWeight="normal">
-                {product.title}
-            </Heading>
-            <Text fontSize="md" mb={6}>
-                ${product.price}
-            </Text>
-            <Text fontSize={'.9rem'} mb={6}>
-                {product.description}
-            </Text>
-            <Heading as="h3" size="md" fontSize={'.9rem'}  mb={6}>
-                DETAILS:
-            </Heading>
-            <VStack align="start" spacing={1} mx={4} mb={6}>
-                {product.details.map((detail, index) => (
-                <Text key={index} fontSize={'.9rem'}>
-                    - {detail}
-                </Text>
-                ))}
-            </VStack>
-            <HStack mb={6} border={'1px solid black'} display="inline-flex">
-                <Button onClick={handleDecreaseQuantity} variant='ghost' borderRadius={0}  _hover={{ bg: 'RGBA(0, 0, 0, 0.24)' }} transition='background-color 0.3s ease'>-</Button>
-                <Text>{quantity}</Text>
-                <Button onClick={handleIncreaseQuantity} variant='ghost' borderRadius={0}  _hover={{ bg: 'RGBA(0, 0, 0, 0.24)' }} transition='background-color 0.3s ease'>+</Button>
-            </HStack>
-            <Button mb={2} onClick={addToCart} w={'100%'} bg='rgb(255, 160, 76)' color='white' variant='outline' _hover={{ bg: 'rgb(230, 137, 50)' }} transition='background-color 0.3s ease' >
-                ADD TO CART
-            </Button>
-            <Link to={`/products`}>
-                <Button  w={'100%'} bg='rgb(227, 130, 52)' color='white' variant='outline' _hover={{ bg: 'rgb(230, 137, 50)' }} transition='background-color 0.3s ease' >
-                    GO BACK
-                </Button>
-            </Link>
-        </Box>
-    </SimpleGrid>
+    <>
+        
+            <SimpleGrid columns={[1, 1, 2]} spacing={10} width={{ xl: '100%', '2xl': '1400px' }} m='0px auto 0px auto' paddingTop={{base: 0, md:'3rem'}} px={3}>
+                <Box width='100%' paddingTop={'2rem'}>
+                <Image
+                    w={'100%'}
+                    h={{base: '25rem', md: '38rem'}}
+                    objectFit={'scale-down'}
+                    src={product.image}
+                    mb={{base: 0, md: '1rem'}}
+                />
+                </Box>
+                <Box >
+                    <Heading as="h2" size={{base: "md", md: 'lg'}} mb={{base: 3, md: 4, lg: 6}} textTransform="uppercase" fontWeight="normal">
+                        {product.title}
+                    </Heading>
+                    <Text fontSize={{base: "sm", md: 'md'}} mb={{base: 3, md: 4, lg: 6}}>
+                        ${product.price}
+                    </Text>
+                    <Text fontSize={'.9rem'} mb={{base: 5, lg: 6}}>
+                        {product.description}
+                    </Text>
+                    <Heading as="h3" size="md" fontSize={'.9rem'}  mb={{base: 3, md: 4, lg: 6}}>
+                        DETAILS:
+                    </Heading>
+                    <VStack align="start" spacing={1} mx={4} mb={{base: 5, lg: 6}}>
+                        {product.details.map((detail, index) => (
+                        <Text key={index} fontSize={'.9rem'}>
+                            - {detail}
+                        </Text>
+                        ))}
+                    </VStack>
+                    <HStack mb={6} border={'1px solid black'} display="inline-flex">
+                        <Button onClick={handleDecreaseQuantity} variant='ghost' borderRadius={0}  _hover={{ bg: 'RGBA(0, 0, 0, 0.24)' }} transition='background-color 0.3s ease'>-</Button>
+                        <Text>{quantity}</Text>
+                        <Button onClick={handleIncreaseQuantity} variant='ghost' borderRadius={0}  _hover={{ bg: 'RGBA(0, 0, 0, 0.24)' }} transition='background-color 0.3s ease'>+</Button>
+                    </HStack>
+                    <Button mb={2} onClick={addToCart} w={'100%'} bg='rgb(255, 160, 76)' color='white' variant='outline' _hover={{ bg: 'rgb(230, 137, 50)' }} transition='background-color 0.3s ease' >
+                        ADD TO CART
+                    </Button>
+                    <Link to={`/products`}>
+                        <Button  w={'100%'} bg='rgb(227, 130, 52)' color='white' variant='outline' _hover={{ bg: 'rgb(250, 127, 70)' }} transition='background-color 0.3s ease' >
+                            GO BACK
+                        </Button>
+                    </Link>
+                </Box>
+            </SimpleGrid>
+       
+    </>
   )
 }
 

5c54f90853b6d617326ef77b93cb1fe8348be1b1
mar69287 <marcoa.ruiz97@gmail.com>
2023-06-29 15:10:59 -0700

    Responsive category and product page

--- src/components/SideBarFilter.jsx
+++ src/components/SideBarFilter.jsx
@@ -1,8 +1,9 @@
-import { VStack, Heading, Text, Input, Button, Flex } from "@chakra-ui/react";
+import { VStack, Heading, Text, Input, Button, Flex, Show, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from "@chakra-ui/react";
 import { Link } from 'react-router-dom';
 
 const SideBarFilter = ({ onPriceRangeChange, setFilteredProducts, products }) => {
   const categories = ["Kitchen", "Clothing"];
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
@@ -16,25 +17,67 @@ const SideBarFilter = ({ onPriceRangeChange, setFilteredProducts, products }) =>
   };
 
   return (
-    <VStack align='start' spacing={4} py={6}>
-      <Heading as="h3" size="md">Categories</Heading>
-      {categories.map(category => (
-        <Link key={category} to={`/products/category/${category}`} onClick={handleLinkClick} >
-          <Text _hover={{ cursor: 'pointer', color: 'rgb(255, 160, 76)', transition: 'color 0.2s ease-in-out' }} paddingLeft={'.4rem'}>{category}</Text>
-        </Link>
-      ))}
-      <Heading as="h3" size="md">Price Range</Heading>
-      <form onSubmit={handleFormSubmit}>
-        <Flex>
-          <Input type="number" name="min" placeholder="$ Min" marginRight="1rem" />
-          <Input type="number" name="max" placeholder="$ Max" marginRight="1rem" />
-          <Button paddingX={'2rem'} type="submit" colorScheme="teal">Apply</Button>
-        </Flex>
-      </form>
-      <Link to={`/products`} onClick={handleLinkClick}>
-        <Text _hover={{ cursor: 'pointer', color: 'rgb(255, 160, 76)', transition: 'color 0.2s ease-in-out' }} paddingLeft={'.4rem'}>View All</Text>
-      </Link>
-    </VStack>
+    <>
+      <Show above="md">
+        <VStack align='start' spacing={4} py={6}>
+          <Heading as="h3" size="md">Categories</Heading>
+          {categories.map(category => (
+            <Link key={category} to={`/products/category/${category}`} onClick={handleLinkClick} >
+              <Text _hover={{ cursor: 'pointer', color: 'rgb(255, 160, 76)', transition: 'color 0.2s ease-in-out' }} paddingLeft={'.4rem'}>{category}</Text>
+            </Link>
+          ))}
+          <Heading as="h3" size="md">Price Range</Heading>
+          <form onSubmit={handleFormSubmit}>
+            <Flex>
+              <Input type="number" name="min" placeholder="$ Min" marginRight="1rem" />
+              <Input type="number" name="max" placeholder="$ Max" marginRight="1rem" />
+              <Button paddingX={'2rem'} type="submit" colorScheme="teal">Apply</Button>
+            </Flex>
+          </form>
+          <Link to={`/products`} onClick={handleLinkClick}>
+            <Text _hover={{ cursor: 'pointer', color: 'rgb(255, 160, 76)', transition: 'color 0.2s ease-in-out' }} paddingLeft={'.4rem'}>View All</Text>
+          </Link>
+        </VStack>
+      </Show>
+      <Show below="lg">
+        <Button onClick={onOpen} mb={'1rem'}>Filters</Button>
+        <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Filters</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Text fontWeight='bold' mb='.5rem'>
+                  Categories
+                </Text>
+                {categories.map(category => (
+                  <Link key={category} to={`/products/category/${category}`} onClick={handleLinkClick} >
+                    <Text _hover={{ cursor: 'pointer', color: 'rgb(255, 160, 76)', transition: 'color 0.2s ease-in-out' }} paddingLeft={'.4rem'} pb={3}>{category}</Text>
+                  </Link>
+                ))}
+                <Text fontWeight='bold' mb='1rem'>
+                  Price Range
+                </Text>
+                <form onSubmit={handleFormSubmit}>
+                  <Flex pb={3}>
+                    <Input type="number" name="min" placeholder="$ Min" marginRight="1rem" />
+                    <Input type="number" name="max" placeholder="$ Max" marginRight="1rem" />
+                    <Button paddingX={'2rem'} type="submit" colorScheme="teal">Apply</Button>
+                  </Flex>
+                </form>
+                <Link to={`/products`} onClick={handleLinkClick}>
+                  <Text mb='1rem' fontWeight={"bold"} _hover={{ cursor: 'pointer', color: 'rgb(255, 160, 76)', transition: 'color 0.2s ease-in-out' }}>View All</Text>
+                </Link>
+                <Flex w={'100%'} justify={"flex-end"}>
+                  <Button colorScheme='blue'  onClick={onClose}>
+                    Close
+                  </Button>
+                </Flex>
+              </ModalBody>
+            </ModalContent>
+        </Modal>
+      </Show>
+    </>
   )
 }
 
--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -39,17 +39,17 @@ const CategoryPage = () => {
   }, [products, priceRange]);
 
   return (
-    <Grid as='section' templateColumns={{ md: 'repeat(6, 1fr)' }} m='1rem auto 3rem auto' width={{ xl: '100%', '2xl': '1400px' }}>
-      <GridItem colSpan={{ md: 2 }} h='100%' borderRight='1px black solid' paddingRight='3rem'>
-        <SideBarFilter onPriceRangeChange={handlePriceRangeChange} setFilteredProducts={setFilteredProducts} products={products} />
-      </GridItem>
-      <GridItem colSpan={{ md: 4 }} paddingLeft='3rem'>
-        <SimpleGrid columns={[1, 2, 3]} spacing={6} w='100%'>
-          {filteredProducts.map(product =>
-            <ProductCard key={product.id} product={product} />
-          )}
-        </SimpleGrid>
-      </GridItem>
+    <Grid as='section' templateColumns={{ md: 'repeat(6, 1fr)', }} m='1rem auto 3rem auto' width={{ xl: '100%', '2xl': '1400px' }}>
+        <GridItem colSpan={{ md: 2}} h={'100%'} borderRight={{ base: 'none', md: '1px solid black' }} paddingRight={'3rem'} pl={{ base: 3}}>
+            <SideBarFilter onPriceRangeChange={handlePriceRangeChange} setFilteredProducts={setFilteredProducts} products={products} />
+        </GridItem>
+        <GridItem colSpan={{ md: 4}} pl={{ base: 0, md: '3rem'}} px={3}>
+            <SimpleGrid  columns={[1, 2, 3]} spacing={6} w={'100%'}>
+                {filteredProducts.map(product => 
+                    <ProductCard key={product.id} product={product} />
+                )}
+            </SimpleGrid >
+        </GridItem>
     </Grid>
   )
 }

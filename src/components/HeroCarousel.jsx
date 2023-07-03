a5b22a04ce4274d294c4c9906f95c35fdc46e00e
mar69287 <marcoa.ruiz97@gmail.com>
2023-07-03 10:04:29 -0700

    Update hero carousel component

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -1,4 +1,3 @@
-import { Box, Flex } from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
 import KitchenCard from './KitchenCard';
 import ClothingCard from './ClothingCard';
@@ -24,48 +23,11 @@ const HeroCarousel = () => {
   };
 
   return (
-    // <Box as='section' w="100%" h="40rem" position="relative" mb='3rem'>
-    //   {cards.map((card, index) => (
-    //     <Box
-    //       key={index}
-    //       position="absolute"
-    //       top={0}
-    //       left={0}
-    //       right={0}
-    //       bottom={0}
-    //       transform={index === activeCard ? 'translateX(0)' : 'translateX(100%)'}
-    //       transition="transform 0.3s ease"
-    //     >
-    //       {card}
-    //     </Box>
-    //   ))}
-
-    //   <Flex justifyContent="center" position="absolute" bottom={4} w="100%">        
-    //     <Box
-    //       w="2"
-    //       h="2"
-    //       borderRadius="full"
-    //       bg={activeCard === 0 ? 'gray.100' : 'gray.400'}
-    //       mx={1}
-    //       cursor="pointer"
-    //       onClick={() => handleDotClick(0)}
-    //       p='.3rem'
-    //     />
-    //     <Box
-    //       w="2"
-    //       h="2"
-    //       borderRadius="full"
-    //       bg={activeCard === 1 ? 'gray.100' : 'gray.400'}
-    //       mx={1}
-    //       cursor="pointer"
-    //       onClick={() => handleDotClick(1)}
-    //       p='.3rem'
-    //     />
-    //   </Flex>
-    // </Box>
-    <Carousel emulateTouch infiniteLoop showArrows={false} autoPlay interval={5000} showStatus={false}>
+    <Carousel emulateTouch infiniteLoop showArrows={false} autoPlay interval={5000} showStatus={false} showThumbs={false}>
       <KitchenCard />
+      
       <ClothingCard />
+     
     </Carousel>
   );
 };

9de0fee7adf1a2f0c10e33f370efadb999a0c7c8
mar69287 <marcoa.ruiz97@gmail.com>
2023-07-03 10:57:57 -0700

    Clean code

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -5,22 +5,6 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const HeroCarousel = () => {
-  const [activeCard, setActiveCard] = useState(0);
-  const cards = [<KitchenCard />, <ClothingCard />];
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setActiveCard((prevCard) => (prevCard === cards.length - 1 ? 0 : prevCard + 1));
-    }, 8000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [activeCard]);
-
-  const handleDotClick = (index) => {
-    setActiveCard(index);
-  };
 
   return (
     <Carousel emulateTouch infiniteLoop showArrows={false} autoPlay interval={5000} showStatus={false} showThumbs={false}>
